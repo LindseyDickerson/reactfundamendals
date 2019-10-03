502df83f84112873741f1f6bb788aa3f42e3c289
Lindsey Dickerson <lindseydickerson2@gmail.com>
2019-10-03 16:01:50 -0400

    day 4

---
+++ Desktop/javascriptLibrary/15-classroom.js
@@ -0,0 +1,26 @@
+// Challenge
+/*
+Create 3 conditional operators to check the type of a variable.
+string vs number vs undefined
+
+string
+number
+boolean 
+console.log "its a string, boolean, this is a number"
+*/
+
+let planted = true;
+if (planted) {
+    console.log(typeof planted);
+};
+
+
+let planted = 'Roses are red';
+if (planted) {
+    console.log(typeof planted);
+};
+
+let boquet = 10;
+if (boquet) {
+    console.log(typeof boquet);
+};
